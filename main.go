876e30fd258c38eb2e7fc9b5ad892f64dd77d65d
Rishita Shaw <rishitashaw49@gmail.com>
2022-07-05 11:14:23 +0530

    router setup

--- main.go
+++ main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"log"
+	"os"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/gin-gonic/gin"
+	"github.com/rishitashaw/ecommerce/controllers"
+	"github.com/rishitashaw/ecommerce/database"
+	"github.com/rishitashaw/ecommerce/middleware"
+	"github.com/rishitashaw/ecommerce/routes"
 )
 
-type JSONTyperesponse struct {
-	Message string
-}
-
 func main() {
-    app := fiber.New()
+    port:=os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    app:=controllers.NewApplication(database.ProductData(database.Client,"Products"), database.UserData(database.Client,"Users"))
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        //return c.SendString("Hello, World!")
-		return c.JSON(JSONTyperesponse{Message: "Hello, Chutiya!"})
-    })
+    router := gin.New()
+    router.Use(middleware.Logger())
 
-    log.Fatal(app.Listen(":8080"))
+    routes.UserRoutes(router)
+    router.Use(middleware.Auth())
+
+    router.GET("/addtocart",app.Addtocart())
+    router.GET("/removeitem",app.RemoveItem())
+    router.GET("/cartcheckout",app.BuyFromCart())
+    router.GET("/instantbuy",app.InstantBuy())
+
+    log.Fatal((router.Run(":"+port)))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,5 +1,14 @@
 package routes
 
-import(
-	""
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rishitashaw/ecommerce/controllers"
 )
+
+func UserRoutes(incomingRoute *gin.Engine){
+	incomingRoute.POST("/users/signup", controllers.Signup())
+	incomingRoute.POST("/users/login", controllers.Login())
+	incomingRoute.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoute.GET("/users/productview", controllers.SearchProduct())
+	incomingRoute.GET("/users/search", controllers.SearchProductByQuery())
+}
